c366307c242a1140b274decb78a7fe7bf301558d
Roger Martinez <rogerthatdev@gmail.com>
2022-07-26 17:20:06 -0700

    flask app runs

--- event-handler/src/event_handler/main.py
+++ event-handler/src/event_handler/main.py
@@ -1,2 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    '''Index page route'''
+
+    return '<h1>Application Deployed!</h1>'
+
 def test():
     return True
+
