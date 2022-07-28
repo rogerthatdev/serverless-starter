e280d701d807a84828aea2525fcc1f5505c65cba
Roger Martinez <rogerthatdev@gmail.com>
2022-07-27 17:16:35 -0700

    event-handler terraform

---
+++ terraform/event-handler.tf
@@ -0,0 +1,60 @@
+resource "google_service_account" "event_handler_cloudbuilder" {
+  account_id = "event-handler-cloud-builder"
+}
+
+resource "google_project_iam_member" "logs_writer_event_handler" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.event_handler_cloudbuilder.email}"
+}
+
+
+resource "google_cloudbuild_trigger" "event_handler_build_trigger" {
+  name = "event-handler-build-trigger"
+
+  build {
+    images = [
+      "us-west2-docker.pkg.dev/citric-sprite-357416/serverless-starter-repo/serverless-starter-event-handler:latest",
+    ]
+    logs_bucket = "${google_storage_bucket.cloudbuild_logs.url}/logs"
+    step {
+      args = [
+        "build",
+        "-t",
+        "${local.artifact_registry_url}/serverless-starter-event-handler:latest",
+        "-f",
+        "Dockerfile",
+        ".",
+      ]
+      dir        = "event-handler"
+      env        = []
+      name       = "gcr.io/cloud-builders/docker"
+      secret_env = []
+      wait_for   = []
+    }
+  }
+
+  github {
+    name  = var.repo_name
+    owner = var.repo_owner
+
+    push {
+      branch       = "main"
+      invert_regex = false
+    }
+  }
+  service_account = google_service_account.event_handler_cloudbuilder.id
+}
+
+resource "google_cloud_run_service" "event_handler" {
+  name     = "serverless-starter-event-handler"
+  location = "us-west2"
+
+  template {
+    spec {
+      containers {
+        image = "${local.artifact_registry_url}/serverless-starter-event-handler:latest"
+      }
+    }
+  }
+}
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,5 +5,8 @@ locals {
     "iam.googleapis.com"
   ]
   artifact_registry_url = format("%s-docker.pkg.dev/%s/%s", google_artifact_registry_repository.docker_images.location, var.project_id, google_artifact_registry_repository.docker_images.name)
-  cloudbuilders         = ["serviceAccount:${google_service_account.frontend_cloudbuilder.email}"]
+  cloudbuilders         = [
+    "serviceAccount:${google_service_account.frontend_cloudbuilder.email}",
+    "serviceAccount:${google_service_account.event_handler_cloudbuilder.email}"
+  ]
 }
